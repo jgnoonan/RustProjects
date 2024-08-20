2c68c07b58dce659787833ac9150d828ba1462ae
Joseph G. Noonan <jgnoonan@att.net>
2024-08-20 12:38:06 -0400

    Added Integers Project

---
+++ integers/src/integers.rs
@@ -0,0 +1,20 @@
+fn main() {
+    /*  Integers in Rust
+        Integers in Rust are numbers withouyt decimals or a number that is not a fraction.
+        Simply put, integers are data types that represent whole numbers.
+
+        Within the umbrella of integers, you have signed and unsigned versions that you can specify
+
+        Signed integers store both negative and postive values
+        Unsigned integers store positive values only
+
+        In addition, the size of an integer can be set to arch.
+        Setting an integer to arch derives the size of the integer from the architecture of the machine, 
+        i.e., if you set if to 64bits then x64 is the machine type.
+
+    */
+
+    let total = 4;
+    
+
+}
