598ce7f5753f7050d591f0c925c72ef20e56dc81
ripple-cny <jgnoonan@att.net>
2024-09-13 11:48:13 -0400

    Operator examples

---
+++ operators/src/operators.rs
@@ -0,0 +1,73 @@
+/*
+What are Operators in Rust
+
+An operator is a character or characters that determine the action that is to be performed or considered.
+The data on which operators are actioned  are called operands.  Consider the following expression -
+a + b = c
+Here, the values of a and be and c are operands, while + and = are operators
+
+The major Operations in Rust to consider are the following:
+
+Arithmetic
+Bitwise
+Comparison
+Logical
+Conditional
+
+Arithmetic Operators
+x = 12
+y = 2
+
+Sr.No   Operator    Description/Example
+1       +(Addition) returns the sum of the operands x+y
+2       -(Subtract) returns the difference of the values x - y
+3       *(Multiply) returns the product of the values x * y
+4       /(Division) performs the division operation and returns the quotient x / y
+5       %(Modulus)  performs the division operation and returns the remainder x % y
+
+Relational Operators
+Relational Operators check or define the relationship equivalency
+between two elements.  Relational operators are used to compare two or more values.
+Relationship operators return a Boolean value: either true or false.
+
+x = 5
+y = 2
+
+1   >   Greater than                (x > y)     returns true
+2   <   Less than                   (x < y)     returns false
+3   >=  Greater than or equal to    (x >= y)    returns true
+4   <=  Less thank or equal to      (x <= y)    returns false
+5   ==  Equality                    (x == y)    returns false
+6   !=  Not equal                   (x != y)    returns true
+
+Logical Operators are used to combine and check two or more conditions.
+
+Logical operators return a Boolean value
+
+x = 1
+y = 2
+
+1   && (AND)    The operator returns true as long as all the expressions specified are true
+                (x > 2 && y > 3) returns false
+2   || (OR)     The operator returns true if at least one of the expressions returns true
+                (x > 2 || x > 3)  returns false
+3   ! (NOT)     The operator returns the inverse of the expression's result
+                !(x < 4) returns false
+*/
+
+
+fn main() {
+
+    let mut sum = 0;
+    let mut dif = 0;
+    let mut prod = 0;
+    let mut quot = 0;
+    let mut modulus = 0;
+    sum = 5 + 9;
+    dif = 9 - 5;
+    prod = 9 * 5;
+    quot = 10 / 5;
+    modulus = 9 % 5;
+
+    println!("{} {} {} {} {}", sum, dif, prod, quot, modulus);
+}
